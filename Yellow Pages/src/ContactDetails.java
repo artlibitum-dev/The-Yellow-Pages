a85f672aa33be9c6328236b6bea56cf29c39303e
artlibitum.dev <artlibitum.dev@gmail.com>
2023-11-10 00:06:04 +0100

    remove presave, add for init, get/set

--- Yellow Pages/src/ContactDetails.java
+++ Yellow Pages/src/ContactDetails.java
@@ -1,88 +1,66 @@
-public class ContactDetails {
-    String firstName;
-    String surname;
-    int    age;
-    String city;
-    int postalCode;
-    String streetName;
-    int    gateNumber;
-    public void Drogba( ){
-        firstName = "Dider";
-        surname ="Drogba";
-        age = 36;
-        city ="London";
-        postalCode = 818;
-        streetName = "Chelsea Street";
-        gateNumber = 11;
-        System.out.println("First Name " + firstName);
-        System.out.println("Surname " +surname);
-        System.out.println("Age " + age);
-        System.out.println("City: " + city + " | Postal Code: " + postalCode + " | Street: " + streetName + " | Gate Number: " + gateNumber);
-    };
-    public void Henry( ){
-        firstName = "Thiery";
-        surname ="Henry";
-        age = 34;
-        city ="London";
-        postalCode = 818;
-        streetName = "Arsenal Street";
-        gateNumber = 14;
-        System.out.println("First Name " + firstName);
-        System.out.println("Surname " +surname);
-        System.out.println("Age " + age);
-        System.out.println("City: " + city + " | Postal Code: " + postalCode + " | Street: " + streetName + " | Gate Number: " + gateNumber);
-    };
-    public void Eto( ){
-        firstName = "Samuel";
-        surname ="Eto";
-        age = 40;
-        city ="Milan";
-        postalCode = 418;
-        streetName = "Inter Street";
-        gateNumber = 29;
-        System.out.println("First Name " + firstName);
-        System.out.println("Surname " +surname);
-        System.out.println("Age " + age);
-        System.out.println("City: " + city + " | Postal Code: " + postalCode + " | Street: " + streetName + " | Gate Number: " + gateNumber);
-    };
-    public void Diouf( ){
-        firstName = "El Hadi";
-        surname ="Diouf";
-        age = 31;
-        city ="Liverpool";
-        postalCode = 348;
-        streetName = "Problem Street";
-        gateNumber = 13;
-        System.out.println("First Name " + firstName);
-        System.out.println("Surname " +surname);
-        System.out.println("Age " + age);
-        System.out.println("City: " + city + " | Postal Code: " + postalCode + " | Street: " + streetName + " | Gate Number: " + gateNumber);
-    };
-    public void Essien( ){
-        firstName = "Dider";
-        surname ="Drogba";
-        age = 36;
-        city ="London";
-        postalCode = 818;
-        streetName = "Chelsea Street";
-        gateNumber = 11;
-        System.out.println("First Name " + firstName);
-        System.out.println("Surname " +surname);
-        System.out.println("Age " + age);
-        System.out.println("City: " + city + " | Postal Code: " + postalCode + " | Street: " + streetName + " | Gate Number: " + gateNumber);
-    };
-    public void Mikel( ){
-        firstName = "John Obi";
-        surname ="Mikel";
-        age = 29;
-        city ="London";
-        postalCode = 818;
-        streetName = "Chelsea Street";
-        gateNumber = 13;
-        System.out.println("First Name " + firstName);
-        System.out.println("Surname " +surname);
-        System.out.println("Age " + age);
-        System.out.println("City: " + city + " | Postal Code: " + postalCode + " | Street: " + streetName + " | Gate Number: " + gateNumber);
-    };
+class ContactDetails {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String city;
+    private int postalCode;
+    private String streetName;
+    private int gateNumber;
+
+    ContactDetails(
+            String firstName,
+            String lastName,
+            int age,
+            String city,
+            int postalCode,
+            String streetName,
+            int gateNumber) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.streetName = streetName;
+        this.gateNumber = gateNumber;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public int getGateNumber() {
+        return gateNumber;
+    }
+
+    public String getAddress() {
+        return ("City: " + city + " | Postal Code: " + postalCode + " | Street: " + streetName + " | Gate Number: "
+                + gateNumber);
+    }
+
+    public String toString() {
+        return "Contacts name: " + firstName + " " + lastName + ", Age: " + age + ", Address: " + getAddress();
+    }
 
 }
