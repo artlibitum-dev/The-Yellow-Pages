54e3acc5a9e676bebbe527c218bfa7a400208a08
artlibitum.dev <artlibitum.dev@gmail.com>
2023-11-10 00:03:06 +0100

    remove files as unnecessary

--- Yellow Pages/src/PhoneContacts.java
+++
@@ -1,9 +0,0 @@
-public class PhoneContacts {
- static String[] preSaveSurnamePhoneList = new String[7];
-
- public static String[] populatepreSaveSurnamePhoneList(){
-  preSaveSurnamePhoneList[0]= "ino";
-  return preSaveSurnamePhoneList;
- }
-
-}
