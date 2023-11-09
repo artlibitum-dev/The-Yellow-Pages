c17e00cd8f14189194e4a58f65f73f8802bc614f
Inotila <78792300+Inotila@users.noreply.github.com>
2023-11-09 14:27:40 +0100

    Called the user function in the menu function
    This is so that there is access to the userType variable and then i can use it to modify the array menu and what the user can see and do.

--- Yellow Pages/src/Menu.java
+++ Yellow Pages/src/Menu.java
@@ -10,13 +10,19 @@ public class Menu {
             "4. Exit"
     };
     public int menuDisplay(){
-//        if (user.equals("Guest user")){
-//           menu = new String[]{
-//                   "1. View contacts",
-//                   "2. Search for contact",
-//                   "3. Exit"
-//           };
-//        }
+        // get users status
+        Users userStatus = new Users();
+        String userRole;
+        userRole = userStatus.userStatus();
+        System.out.println("Your role is: " + userRole + "\n");
+
+        if (userRole.equals("Guest user")){
+           menu = new String[]{
+                   "1. View contacts",
+                   "2. Search for contact",
+                   "3. Exit"
+           };
+        }
         //Menu options
         while (true) {
             try {
@@ -24,14 +30,23 @@ public class Menu {
                 for (int i=0; i < menu.length; i++ ) {
                     System.out.println(menu[i]);
                 }
-                System.out.println( "Select your your option by entering a number from 1 - 4");
-                userMenuChoice = scanner.nextInt();
+                if (userRole.equals("Guest user")){
+                System.out.println( "Select your your option by entering a number from 1 - 3");
+                userMenuChoice = scanner.nextInt();}
+                else {
+                    System.out.println( "Select your your option by entering a number from 1 - 4");
+                }
 
                 if (userMenuChoice < menu.length+1 ) {
                     System.out.println(menu[--userMenuChoice]);
                     return userMenuChoice;
                 } else {
-                    System.out.print("Invalid entry! You enter: " + userMenuChoice + " is not a number between 1-4!" + "\n");
+                    if (userRole.equals("Guest user")) {
+                        System.out.print("Invalid entry! You enter: " + userMenuChoice + " is not a number between 1-3!" + "\n");
+                    } else {
+                        System.out.print("Invalid entry! You enter: " + userMenuChoice + " is not a number between 1-4!" + "\n");
+
+                    }
                 }
             } catch (Exception e) {
                 System.out.println("Error: Input is not an number."+ "\n");
