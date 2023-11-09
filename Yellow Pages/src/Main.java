7a4d63f9dcb2921baf39eef8a0d12bb3ea26f9d0
artlibitum.dev <artlibitum.dev@gmail.com>
2023-11-10 00:06:58 +0100

    clean up Main

--- Yellow Pages/src/Main.java
+++ Yellow Pages/src/Main.java
@@ -1,4 +1,5 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -8,42 +9,37 @@ public class Main {
         // welcome message
         System.out.println("Welcome to Yellow Pages!" + "\n");
 
-
-
-
-
-
-
         //display menu
         Menu menuDisplay = new Menu();
         menuDisplay.menuDisplay();
         int userMenuChoice = menuDisplay.userMenuChoice;
 
-        // preSaved contact list
-        String[] preSaveSurnamePhoneList = new String[7];
-        preSaveSurnamePhoneList[0]= "Drogba";
-        preSaveSurnamePhoneList[1]= "Henry";
-        preSaveSurnamePhoneList[2]= "Eto";
-        preSaveSurnamePhoneList[3]= "Diouf";
-        preSaveSurnamePhoneList[4]= "Essien";
-        preSaveSurnamePhoneList[5]= "Mikel";
-        preSaveSurnamePhoneList[6]= "";
+       
+        YellowPages yellowPages = new YellowPages();
 
-        ContactDetails contactDetails = new ContactDetails();
-        if (userMenuChoice == 1){
-            System.out.println("Enter the number of the contact you would like to view:");
-            for (int i = 0; i < preSaveSurnamePhoneList.length; i++)
-                System.out.println((i +1)+". " + preSaveSurnamePhoneList[i]);
+        if (userMenuChoice == 0) /* Exit */ {
+            System.out.println("Exiting: Bye Bye");
+            System.exit(0);
+        } 
+            else if (userMenuChoice == 1) /* View */ {
+            yellowPages.viewContacts();
+        } 
+            else if (userMenuChoice == 2) /* Search */ {
+        
+        } 
+            else if (userMenuChoice == 3) /* Edit */ {
+        
+        } 
+            else if (userMenuChoice == 5) /* Add */ {
 
-            //Open contact details
-        int openContactDetails = 0;
-            System.out.println("Pick which number you would like to open");
-            // openContactDetails = scanner.nextInt();
-            if (openContactDetails == 1){
-                contactDetails.Drogba();
-            }
         }
+            else if (userMenuChoice == 6) /* Remove */ {
+
+        }
+
+        }    
+
+        // 
 
         //display contact details
     }
-}
