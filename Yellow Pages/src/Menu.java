368bc7ce2a56534cba6c66b522f140db55309514
artlibitum.dev <artlibitum.dev@gmail.com>
2023-11-09 17:04:27 +0100

    cleaner way to show menu!

--- Yellow Pages/src/Menu.java
+++ Yellow Pages/src/Menu.java
@@ -3,12 +3,26 @@ import java.util.Scanner;
 public class Menu {
     int userMenuChoice;
     Scanner scanner = new Scanner(System.in);
-    String[] menu = {
-            "1. View contacts",
-            "2. Search for contact",
-            "3. Edit contacts",
-            "4. Exit"
-    };
+
+    private String[] getMenu(String userRole) {
+        String[] menu;
+        if (userRole == "Guest user") {
+            menu = new String[]  {
+                "1. View contacts",
+                "2. Search for contact",
+                "3. Exit"
+            };
+        } else {
+            menu = new String[]  {
+                "1. View contacts",
+                "2. Search for contact",
+                "3. Edit contacts",
+                "4. Exit"
+            };
+        }
+        return menu;
+    }
+    
     public int menuDisplay(){
         // get users status
         Users userStatus = new Users();
@@ -16,35 +30,28 @@ public class Menu {
         userRole = userStatus.userStatus();
         System.out.println("Your role is: " + userRole + "\n");
 
-        if (userRole.equals("Guest user")){
-           menu = new String[]{
-                   "1. View contacts",
-                   "2. Search for contact",
-                   "3. Exit"
-           };
-        }
         //Menu options
         while (true) {
             try {
                 System.out.println("Main menu:");
+
+                String[] menu = getMenu(userRole);
+
+                System.out.println( "Select your your option by entering a number from 1 - " + menu.length);
                 for (int i=0; i < menu.length; i++ ) {
                     System.out.println(menu[i]);
                 }
-                if (userRole.equals("Guest user")){
-                System.out.println( "Select your your option by entering a number from 1 - 3");
-                userMenuChoice = scanner.nextInt();}
-                else {
-                    System.out.println( "Select your your option by entering a number from 1 - 4");
-                }
+                userMenuChoice = scanner.nextInt();
+
 
                 if (userMenuChoice < menu.length+1 ) {
-                    System.out.println(menu[--userMenuChoice]);
+                    System.out.println(menu[userMenuChoice - 1]);
                     return userMenuChoice;
                 } else {
                     if (userRole.equals("Guest user")) {
-                        System.out.print("Invalid entry! You enter: " + userMenuChoice + " is not a number between 1-3!" + "\n");
+                        System.out.print("Invalid entry! You enter: " + userMenuChoice + " is not a number between 1 - " + menu.length + "!\n");
                     } else {
-                        System.out.print("Invalid entry! You enter: " + userMenuChoice + " is not a number between 1-4!" + "\n");
+                        System.out.print("Invalid entry! You enter: " + userMenuChoice + " is not a number between 1 -" + menu.length + "!\n");
 
                     }
                 }
